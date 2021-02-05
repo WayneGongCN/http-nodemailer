2581e5518d4cc7d23748abdefadb30d44ee1d2fa
ubuntu <ubuntu@localhost.localdomain>
2021-02-05 17:37:58 +0800

    update

--- app.js
+++ app.js
@@ -11,19 +11,18 @@ app.use(express.urlencoded({ extended: false }));
 
 app.post('/', function (req, res, next) {
   const { smtp, message } = req.body
-  console.log(smtp, message)
-
   try {
     nodemailer.createTransport(smtp)
-    .sendMail(message, (e, result) => {
-      console.log(e)
-      if (e) return next(e);
-      res.json(result)
-    });
+		.sendMail(message, (e, result) => {
+    	  console.log(e)
+    	  if (e) return next(e);
+    	  res.json(result)
+    	});
   } catch (e) {
     next(e)
   }
 });
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -34,6 +33,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.error(err)
 
   // render the error page
   res.status(err.status || 500);
